49d6527f71d8dcc1ddc37393bde7123c416efee1
Tadeo Wotoszyn <tadeowoto@MacBook-Pro-de-Tadeo.local>
2024-05-17 12:30:20 -0300

    borrar inscripcion por los id de la materia y de el alumno agregado

--- src/universidadgrupo1/AccesoADatos/InscripcionData.java
+++ src/universidadgrupo1/AccesoADatos/InscripcionData.java
@@ -1,6 +1,8 @@
 
 package universidadgrupo1.AccesoADatos;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import universidadgrupo1.entidades.Inscripcion;
 
@@ -44,10 +46,60 @@ public class InscripcionData {
            
        } catch (SQLException ex) {
            JOptionPane.showMessageDialog(null, "error, no se pudo acceder a la tabla inscripcion");
-       }
-        
-        
+       } 
     }
     
+    public void actualizarNota(int idAlumno, int idMateria, double nota){
+    
+        String sql = "UPDATE inscripcion SET nota = ? WHERE idAlumno = ? AND idMateria = ?";
+        
+        try {
+           PreparedStatement ps = c.prepareStatement(sql);
+           
+           ps.setDouble(1, nota);
+           ps.setInt(2,idAlumno);
+           ps.setInt(3,idMateria);
+           
+           int validation = ps.executeUpdate();
+           if(validation == 1){
+                JOptionPane.showMessageDialog(null, "La nota fue actualizada!");
+            }
+            ps.close();
+
+           
+       } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "error, no se pudo acceder a la tabla, no se pudo cambiar la nota");
+       } 
+    }
+      
+    
+    
+    public void borrarInscripcionIDAlumnoIDMateria(int idAlumno, int idMateria){
+       try {
+           //TAMBIEN SE PUEDE BORRAR LA INSCRIPCION CON EL ID DE LA INSCRIPCION
+           //de esta forma tambien se puede hacer ya que un alumno no se puede anotar 2 veces a la misma materia
+           
+           String sql = "DELETE FROM inscripcion WHERE idAlumno = ? AND idMateria = ?";
+           PreparedStatement ps = c.prepareStatement(sql);
+           ps.setInt(1, idAlumno);
+           ps.setInt(2, idMateria);
+           
+           int validation = ps.executeUpdate();
+           
+           if(validation == 1){
+                JOptionPane.showMessageDialog(null, "Se borro esa inscipcion!");
+            }
+            ps.close();
+           
+       } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "error, no se pudo acceder a la tabla, no se pudo borrar la inscripcion");
+       }
+    
+    }
     
 }
+    
+    
+    
+    
+
